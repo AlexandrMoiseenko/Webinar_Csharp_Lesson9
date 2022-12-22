e77b7db0ca4ab2d77212c818b8f14309a10fdaac
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-22 19:57:50 +0300

    программа по нахождению суммы чисел в промежутке от  до N

---
+++ Ex_002/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения М и N. Напишите программу, которая найдет сумму натуральных
+// элементов в промежутке от М до N с помощью рекурсии.
+
+int Promt(string message)                        
+{
+    Console.Write(message);                 
+    return Convert.ToInt32(Console.ReadLine());    
+}
+
+int Sum(int numberStart, int numberFinish)
+{
+    if(numberStart == numberFinish)
+    {
+        return numberStart;
+    }
+    return numberStart + Sum(numberStart + 1, numberFinish);
+}
+
+int numberStart = Promt("Введите начальное число: ");
+int numberFinish = Promt("Введите конечное число: ");
+Console.WriteLine(Sum(numberStart, numberFinish));
