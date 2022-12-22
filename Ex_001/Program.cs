e4abf9d7965a54589de355b26749a4c66afc0321
AlexandrMoiseenko <moiseenko.112@mail.ru>
2022-12-22 19:40:47 +0300

    программа по выводу четных натуральных чисел

---
+++ Ex_001/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения М и N. Напишите программу, которая выведет
+// все четные натуральные числа в промежутке от М до N с помощью
+// рекурсии.
+
+int Promt(string message)                        
+{
+    Console.Write(message);                 
+    return Convert.ToInt32(Console.ReadLine());    
+}
+
+void PrintNumbers(int number)
+{
+    if(number < 1)                   
+    {
+        return;
+    } 
+    if(number%2 == 0)
+    {
+    PrintNumbers(number - 2);
+    Console.Write($"{number}; ");
+    }
+    else 
+    {
+        number -= 1;
+        PrintNumbers(number - 2);
+        Console.Write($"{number}; ");
+    }
+}
+
+int number = Promt("Введите число: ");
+PrintNumbers(number);
